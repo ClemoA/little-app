73c2e8eaf246b533cec5ba7a00bb4aa58f4f7129
Clementine <clementine.amouroux@epitech.eu>
2025-12-12 20:29:21 +0100

    initial commit.

---
+++ src/main/java/com/clementine_amouroux/little_app/LittleAppApplication.java
@@ -0,0 +1,13 @@
+package com.clementine_amouroux.little_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clementine_amouroux/little_app/LittleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clementine_amouroux.little_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
